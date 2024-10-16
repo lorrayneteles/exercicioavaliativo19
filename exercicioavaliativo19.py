43d816b9533735b779d99dbfea881fb37bdc2513
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-16 18:44:50 -0300

    Add files via upload

---
+++ exercicioavaliativo19.py
@@ -0,0 +1,7 @@
+#Quantidade de segundos em um dia
+
+dias = int(input("Digite a quantidade de dias:"))
+
+segundos_dia = dias * 24 * 60 * 60
+
+print(f'A quantidade de dias em segundos é:', segundos_dia,'segundos')
